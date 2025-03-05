4b58cee919a67ce7d2463b5fd4ce8600e3cea10e
Ernestas Vaiciukevicius <e.vaiciukevicius@adform.com>
2025-03-05 17:36:28 +0200

    Adopt for Android14 build

--- fermata/src/main/java/me/aap/fermata/ui/fragment/FoldersFragment.java
+++ fermata/src/main/java/me/aap/fermata/ui/fragment/FoldersFragment.java
@@ -23,7 +23,7 @@ import android.view.animation.AnimationUtils;
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
 
-import com.google.android.play.core.install.InstallException;
+
 import com.google.android.play.core.splitinstall.SplitInstallException;
 
 import java.util.List;
@@ -215,7 +215,7 @@ public class FoldersFragment extends MediaLibFragment {
 			String n = getString(name);
 			Log.e(ex, "Failed to add folder: ", name);
 
-			if (ex instanceof InstallException || ex instanceof SplitInstallException) {
+			if (ex instanceof SplitInstallException) {
 				UiUtils.showAlert(getContext(), getString(R.string.err_failed_install_module, n));
 			} else {
 				String msg = ex.getLocalizedMessage();
